af209854dd4fa3e3c79a2a9b44dc4f7ef8a8b692
Juan Villa <jvill169@fiu.edu>
2025-03-22 21:23:48 -0400

    Added doctor controller

--- src/controllers/doctors.controller.js
+++ src/controllers/doctors.controller.js
@@ -11,6 +11,45 @@ function getDoctorsAll(req, res) {
     });
 }
 
+function putDoctorData(req, res) {
+
+    const paramId = req.params.id;
+
+    const dataToUpdate = {
+        specialty: req.body.specialty,
+        department: req.body.department,
+        license_number: req.body.license_number,
+        biography: req.body.biography || null,
+        education: req.body.education,
+        image_url: req.body.image_url || null,
+        rating: req.body.rating || null,
+        review_count: req.body.review_count || null
+    }
+
+    model.Doctor.findOne({where: {user_Id: paramId}}).then(user => {
+
+        if(user === null){
+            res.status(401).json({
+                message:"UserId for doctor not found."
+            });
+        } else {
+
+            model.Doctor.update(dataToUpdate, {where: {user_Id: paramId}}).then( result => {
+                res.status(200).json({
+                    message: "Updated doctor data successfully",
+                    updatedData: dataToUpdate
+                })
+            }).catch( error => {
+                res.status(500).json({
+                    message: "Something went wrong when updating the doctor data",
+                    error: error
+                })
+            })
+        }
+    })
+}
+
 module.exports = {
-    getDoctorsAll: getDoctorsAll
+    getDoctorsAll: getDoctorsAll,
+    putDoctorData: putDoctorData
 }
--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -4,5 +4,6 @@ const doctorController = require('../controllers/doctors.controller');
 const router = express.Router();
 
 router.get('/', doctorController.getDoctorsAll);
+router.put('/:id',doctorController.putDoctorData);
 
 module.exports = router;
